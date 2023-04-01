506096ec19fa71e55efd16e6d87140991e2d83b0
Mahesh <maheshsolanke1122@gmail.com>
2023-04-01 11:47:59 +0530

    added style

---
+++ build/precache-manifest.422fd1666ecdc894ba726326e1714931.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e5729a319f31fd226320e66e3907498",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22f68b29e13b1143db3f",
+    "url": "/static/css/main.9ee91a79.chunk.css"
+  },
+  {
+    "revision": "45fd5836b82a21efa67c",
+    "url": "/static/js/2.d8eb43be.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.d8eb43be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22f68b29e13b1143db3f",
+    "url": "/static/js/main.068c151f.chunk.js"
+  },
+  {
+    "revision": "97c002b8f8a25334b102",
+    "url": "/static/js/runtime-main.67d84520.js"
+  }
+]);
